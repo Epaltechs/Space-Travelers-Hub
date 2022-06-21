9f70075d67570dda2d84e79e819d867594f7bdd3
Emmanuel Paul <91189703+Epaltechs@users.noreply.github.com>
2022-06-21 15:19:51 +0100

    Edit index.js file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './redux/configureStore';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
