1acd5f4a28ddcb3cbc16d2f66cfd3cd05fbccf64
Emmanuel Paul <91189703+Epaltechs@users.noreply.github.com>
2022-06-22 17:55:14 +0100

    Corrections & Fix Linter Errors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-// import './styles/MyProfile.css';
-// import './styles/Rockets.css';
+import './styles/MyProfile.css';
 import './styles/Missions.css';
 import './styles/Header.css';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import MyProfile from './components/MyProfile';
-import Rockets from './components/Rockets';
 import Missions from './components/Missions';
 
 function App() {
@@ -13,12 +11,6 @@ function App() {
     <>
       <Header />
       <Routes>
-        <Route
-          path="/"
-          element={(
-            <Rockets />
-        )}
-        />
         <Route
           path="/missions"
           element={(
--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import MissionProfile from './missionsComponent/missionsProfile';
-import RocketProfile from './rocketsComponent/RocketProfile';
-import MissionProfile from './missionsComponent/missionsProfile';
 
 const MyProfile = () => (
 
   <main className="profile-main">
     <MissionProfile />
-    <RocketProfile />
   </main>
 );
 
