7b54836f38834a96c2a369d267ee299ca6722fe8
HS Mathebula <mathebulahappen8@gmail.com>
2022-06-22 18:51:47 +0200

    Update MyProfile.js

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import MissionProfile from './missionsComponent/missionsProfile';
 import RocketProfile from './rocketsComponent/RocketProfile';
-import MissionProfile from './missionsComponent/missionsProfile';
 
 const MyProfile = () => (
 
