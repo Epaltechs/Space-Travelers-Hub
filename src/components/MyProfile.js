5c874aaa54bd6d9204648e0f4c5f00a355b74b81
Emmanuel Paul <91189703+Epaltechs@users.noreply.github.com>
2022-06-22 16:54:03 +0100

    Final styling to the missions components

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import MissionProfile from './missionsComponent/missionsProfile';
 import RocketProfile from './rocketsComponent/RocketProfile';
 
 const MyProfile = () => (
 
   <main className="profile-main">
+    <MissionProfile />
     <RocketProfile />
   </main>
 );
