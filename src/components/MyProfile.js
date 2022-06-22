6b70963aea3180848ce8fe612d9605633ca95452
Emmanuel Paul <91189703+Epaltechs@users.noreply.github.com>
2022-06-22 17:17:35 +0100

    Corrections & Fix Linter Errors

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import MissionProfile from './missionsComponent/missionsProfile';
+import RocketProfile from './rocketsComponent/RocketProfile';
 
 const MyProfile = () => (
 
-  <main className="profile-main" />
+  <main className="profile-main">
+    <MissionProfile />
+    <RocketProfile />
+  </main>
 );
 
 export default MyProfile;
