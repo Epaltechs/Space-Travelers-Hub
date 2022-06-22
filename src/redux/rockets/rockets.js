f1820b29e96b59bbf3b46bc7ac4c5f4f7e112584
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-06-22 12:18:01 +0200

    Fetch rockets - Fetch data

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,4 +1,4 @@
-// import { Axios } from 'axios';
+import { Axios } from 'axios';
 
 const FETCH_ROCKET = 'space_travelers_hub/rockets/FETCH_ROCKET';
 const BOOK_ROCKET = 'space_travelers_hub/rockets/BOOK_ROCKET';
@@ -20,5 +20,19 @@ export default bookRocket;
 //   APIs-functions
 
 export const fetchRocketApi = () => async (dispatch) => {
-  dispatch(fetchRocket());
+  const returnValue = await Axios.get('https://api.spacexdata.com/v3/rockets');
+  const { data } = returnValue;
+  const rockets = [];
+  for (let i = 0; i < data.length; i += 1) {
+    const name = data[i].rocket_name;
+    const { id } = data[i];
+    const { description } = data[i];
+    const image = data[i].flickr_images[0];
+    const reserved = false;
+    const object = {
+      id, name, description, image, reserved,
+    };
+    rockets.push(object);
+  }
+  dispatch(fetchRocket(rockets));
 };
