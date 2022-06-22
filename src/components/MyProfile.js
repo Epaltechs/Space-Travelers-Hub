5da819ebbbb7b0d890e7c30208d07db79f484516
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-06-22 17:54:18 +0200

    Rectoring code

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import RocketProfile from './rocketsComponent/RocketProfile';
+import MissionProfile from './missionsComponent/missionsProfile';
 
 const MyProfile = () => (
 
   <main className="profile-main">
+    <MissionProfile />
     <RocketProfile />
   </main>
 );
